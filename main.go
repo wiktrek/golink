61d740f59c80e4462da1821d30ae0e4e7aadd79d
wiktrek <wiktor.janiszewski10@gmail.com>
2025-08-03 17:00:22 +0200

    feat: case "DELETE" added, empty functions for all methods

--- main.go
+++ main.go
@@ -6,6 +6,17 @@ import (
 	"net/http"
 )
 
+/*
+	Create DB
+	Add methods for Link
+		Get types:
+			redirect
+			name
+			owner
+	Add methods for User
+		Get  types:
+			Links
+*/
 func main() {
 
 
@@ -18,6 +29,7 @@ func userHandler(w http.ResponseWriter, r *http.Request) {
     switch r.Method {
     case "GET":
     case "POST":
+	case "DELETE":
     default:
         http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
     }
@@ -26,7 +38,27 @@ func linkHandler(w http.ResponseWriter, r *http.Request) {
     switch r.Method {
     case "GET":
     case "POST":
+	case "DELETE":
     default:
         http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
     }
 }
+func get_link() {
+
+}
+func delete_link() {
+
+}
+func create_link() {
+
+}
+func get_user() {
+
+}
+func delete_user() {
+
+}
+func create_user() {
+
+}
+
