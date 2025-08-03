d2afbb600fb5f381f6d9e61cbded47758b72ab32
wiktrek <wiktor.janiszewski10@gmail.com>
2025-08-03 16:51:39 +0200

    feat: userHandler, linkHander

--- main.go
+++ main.go
@@ -9,6 +9,24 @@ import (
 func main() {
 
 
+    http.HandleFunc("/api/user/", userHandler)
+	http.HandleFunc("/api/link/", linkHandler)
     fmt.Println("Server is running at http://localhost:8080")
     log.Fatal(http.ListenAndServe(":8080", nil))
+}
+func userHandler(w http.ResponseWriter, r *http.Request) {
+    switch r.Method {
+    case "GET":
+    case "POST":
+    default:
+        http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+    }
+}
+func linkHandler(w http.ResponseWriter, r *http.Request) {
+    switch r.Method {
+    case "GET":
+    case "POST":
+    default:
+        http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+    }
 }
---
+++ readme.md
@@ -0,0 +1,9 @@
+# plans:
+
+- create API endpoints: create link, account. Account will have usernames + passwords no email stuff no resetting passwords for now.
+- create simple webpage for sending requests
+- automate deleting short links
+- make golink work
+- make golink pretty
+- SQLite db??
+- host it???
